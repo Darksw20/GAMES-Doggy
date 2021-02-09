cc4543d0fc167309bfc4fa9a6c1e2bd070aef0fe
Rodrigo Zamora <rodrigo_zamora_dav@hotmail.com>
2021-02-08 20:24:45 -0600

    added indexation

---
+++ Doggy/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void NewGame()
+    {
+        SceneManager.LoadScene("NewGame");
+    }
+
+    public void HowToPlay()
+    {
+        SceneManager.LoadScene("HowTo");
+    }
+
+    public void Sound()
+    {
+        SceneManager.LoadScene("Sound");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
