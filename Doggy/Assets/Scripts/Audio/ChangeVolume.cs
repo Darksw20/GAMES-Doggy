7c64a5474787fdf390222976ad702cbe9d9a860d
Rodrigo Zamora <rodrigo_zamora_dav@hotmail.com>
2021-02-09 21:08:49 -0600

    added sound menu

---
+++ Doggy/Assets/Scripts/Audio/ChangeVolume.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeVolume : MonoBehaviour
+{
+    public void SetMusic()
+    {
+        if(AudioListener.pause == false)
+        {
+            AudioListener.pause = true;
+        } else
+        {
+            AudioListener.pause = false;
+        }
+    }
+}
