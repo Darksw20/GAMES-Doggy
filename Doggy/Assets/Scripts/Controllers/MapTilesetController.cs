c19127c83c42d7a244bb58143a8f20351d7d14ea
Darksw20 <ricardog.navi20@gmail.com>
2021-05-25 16:38:30 -0500

    map 2 random stuff added to the laberynth

--- Doggy/Assets/Scripts/Controllers/MapTilesetController.cs
+++ Doggy/Assets/Scripts/Controllers/MapTilesetController.cs
@@ -9,14 +9,34 @@ public class MapTilesetController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        maps[0].SetActive(false);
-        maps[1].SetActive(false);
-        maps[2].SetActive(true);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        int num = (int)Random.Range(0.0f, 2.0f);
+        int i = (int)Random.Range(1.0f, 12.0f);
+        for (;i > 0; i--)
+        {
+            num = (int)Random.Range(0.0f, 3.0f);
+        }
+        switch (num.ToString())
+        {
+            case "0":
+                maps[0].SetActive(true);
+                maps[1].SetActive(false);
+                maps[2].SetActive(false);
+                break;
+            case "1":
+                maps[0].SetActive(false);
+                maps[1].SetActive(true);
+                maps[2].SetActive(false);
+                break;
+            case "2":
+                maps[0].SetActive(false);
+                maps[1].SetActive(false);
+                maps[2].SetActive(true);
+                break;
+            default:
+                maps[0].SetActive(true);
+                maps[1].SetActive(false);
+                maps[2].SetActive(false);
+                break;
+        }
     }
 }
