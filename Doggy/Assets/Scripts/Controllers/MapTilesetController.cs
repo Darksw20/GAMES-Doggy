f9219d5c6a140f531cca806a27d814571bffb0dc
Darksw20 <ricardog.navi20@gmail.com>
2021-05-25 15:10:40 -0500

    GameLoader added

---
+++ Doggy/Assets/Scripts/Controllers/MapTilesetController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapTilesetController : MonoBehaviour
+{
+    public GameObject[] maps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maps[0].SetActive(true);
+        maps[1].SetActive(true);
+        maps[1].SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
