acb9515f4234f3d9976fefae1587cae911b0e9e6
Rodrigo Zamora <rodrigo_zamora_dav@hotmail.com>
2021-05-30 15:34:47 -0500

    Ana dance animations and meta changes
    Level3_1

---
+++ Doggy/Assets/Scripts/Controllers/lvl3_1_danceController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lvl3_1_danceController : MonoBehaviour
+{
+
+    public GameObject ana;
+    //public GameObject perrero;
+
+    private Animator anaAnimator;
+    //private Animator perreroAnimator;
+
+    private Vector2 movement;
+
+    void Start()
+    {
+        anaAnimator = ana.GetComponent<Animator>();
+        //perreroAnimator = perrero.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        anaAnimator.SetFloat("x", movement.x);
+        anaAnimator.SetFloat("y", movement.y);
+    }
+}
