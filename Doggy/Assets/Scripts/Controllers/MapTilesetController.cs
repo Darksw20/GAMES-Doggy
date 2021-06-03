9a353a4ffd3e4b82b1169e4a956d659316d2541c
Rodrigo Zamora <rodrigo_zamora_dav@hotmail.com>
2021-06-03 09:55:45 -0500

    Fixed bug with map not loading
    Level1_2

--- Doggy/Assets/Scripts/Controllers/MapTilesetController.cs
+++ Doggy/Assets/Scripts/Controllers/MapTilesetController.cs
@@ -4,38 +4,33 @@ using UnityEngine;
 
 public class MapTilesetController : MonoBehaviour
 {
-    public GameObject[] maps;
+    public GameObject _1map;
+    public GameObject _2map;
+    public GameObject _3map;
 
-    // Start is called before the first frame update
     void Start()
     {
-        int num = (int)Random.Range(0.0f, 2.0f);
-        int i = (int)Random.Range(1.0f, 12.0f);
-        for (;i > 0; i--)
-        {
-            num = (int)Random.Range(0.0f, 3.0f);
-        }
+        int num = (int)Random.Range(0.0f, 3.0f);
+        Debug.Log("num: " + num);
         switch (num.ToString())
         {
             case "0":
-                maps[0].SetActive(true);
-                maps[1].SetActive(false);
-                maps[2].SetActive(false);
+                Debug.Log("case 0");
+                _1map.SetActive(true);
+                _2map.SetActive(false);
+                _3map.SetActive(false);
                 break;
             case "1":
-                maps[0].SetActive(false);
-                maps[1].SetActive(true);
-                maps[2].SetActive(false);
+                Debug.Log("case 1");
+                _1map.SetActive(false);
+                _2map.SetActive(true);
+                _3map.SetActive(false);
                 break;
             case "2":
-                maps[0].SetActive(false);
-                maps[1].SetActive(false);
-                maps[2].SetActive(true);
-                break;
-            default:
-                maps[0].SetActive(true);
-                maps[1].SetActive(false);
-                maps[2].SetActive(false);
+                Debug.Log("case 2");
+                _1map.SetActive(false);
+                _2map.SetActive(false);
+                _3map.SetActive(true);
                 break;
         }
     }
