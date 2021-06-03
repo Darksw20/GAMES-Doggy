d3e9fc0a3e2572c1948d3bee54702ee84887a572
Rodrigo Zamora <rodrigo_zamora_dav@hotmail.com>
2021-06-03 01:02:27 -0500

    Added pineCones
    Still missing fall animation and check collision with Ana

---
+++ Doggy/Assets/Scripts/Controllers/Levels/Level1_2/level1_2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class level1_2 : MonoBehaviour
+{
+    public GameObject Ana;
+
+    public GameObject _1pineCone;
+    public GameObject _2pineCone;
+    public GameObject _3pineCone;
+    public GameObject _4pineCone;
+    public GameObject _5pineCone;
+
+    void Start()
+    {
+        InvokeRepeating("pineConesRandomPosition", 5F, 5F);
+    }
+
+    public void resetLevel()
+    {
+        
+    }
+
+    private void pineConesRandomPosition()
+    {
+        _1pineCone.GetComponent<pineCone>().changePosition();
+        _2pineCone.GetComponent<pineCone>().changePosition();
+        _3pineCone.GetComponent<pineCone>().changePosition();
+        _4pineCone.GetComponent<pineCone>().changePosition();
+        _5pineCone.GetComponent<pineCone>().changePosition();
+    }
+}
---
+++ Doggy/Assets/Scripts/Controllers/Levels/Level1_2/pineCone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pineCone : MonoBehaviour
+{
+    private bool collisionDetection = true;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Mapa")
+        {
+            collisionDetection = false;
+            changePosition();
+        } else if (collision.gameObject.tag == "Player")
+        {
+            GameManager.instancia.health--;
+            GameObject.Find("Maps").GetComponent<level1_2>().resetLevel();
+        }
+    }
+
+    public void changePosition()
+    {
+        float y = Random.Range(-3.5F, 4.5F);
+        transform.localPosition = new Vector3(Random.Range(-9F, 9F), y, 0);
+    }
+}
