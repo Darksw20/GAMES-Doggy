bdf3c48d9c54dde736ba350a4c3bc2d709542f8a
Rodrigo Zamora <rodrigo_zamora_dav@hotmail.com>
2021-06-03 15:16:19 -0500

    Added "Heladero" vehicle

---
+++ Doggy/Assets/Scripts/Characters/TopDownVehicleMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownVehicleMovement : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+
+    private Rigidbody2D rigidbody;
+    private Vector2 movement;
+
+    void Start()
+    {
+        movement.x = 0.1F;
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
