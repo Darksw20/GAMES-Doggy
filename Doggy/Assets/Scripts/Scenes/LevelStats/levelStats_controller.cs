e3ee24a029703e0306d192bb209e479a46078614
Rodrigo Zamora <rodrigo_zamora_dav@hotmail.com>
2021-06-05 17:27:52 -0500

    Added stats scene

---
+++ Doggy/Assets/Scripts/Scenes/LevelStats/levelStats_controller.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelStats_controller : MonoBehaviour
+{
+    public int WIN = 0;
+    public int LOSE = 0;
+
+    void Start()
+    {
+                
+    }
+
+}
