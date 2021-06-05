e6d6f00dad2b9db66f28034bc944d9ea3a466d21
Rodrigo Zamora <rodrigo_zamora_dav@hotmail.com>
2021-06-05 17:46:31 -0500

    Added stats script

--- Doggy/Assets/Scripts/Scenes/LevelStats/levelStats_controller.cs
+++ Doggy/Assets/Scripts/Scenes/LevelStats/levelStats_controller.cs
@@ -1,15 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class levelStats_controller : MonoBehaviour
 {
-    public int WIN = 0;
-    public int LOSE = 0;
+    public TextMeshProUGUI time;
+    public TextMeshProUGUI level;
+    public TextMeshProUGUI health;
+    public TextMeshProUGUI redDiamonds;
+    public TextMeshProUGUI blueDiamonds;
+    public TextMeshProUGUI coins;
+
+    private static string currentScene = null;
 
     void Start()
     {
-                
+        updateText();
+    }
+
+    private void updateText()
+    {
+        time.text = "Completado en:\n" + GameManager.instancia.time.ToString() + " segundos";
+        level.text = "misión";
+        health.text = GameManager.instancia.health.ToString();
+        redDiamonds.text = GameManager.instancia.redJewels.ToString();
+        blueDiamonds.text = GameManager.instancia.blueJewels.ToString();
+        coins.text = GameManager.instancia.money.ToString();
+    }
+
+    public static void setCurrentScene(string scene)
+    {
+        currentScene = scene;
+    }
+
+    public void nextScene()
+    {
+        Debug.Log("next scene");
     }
 
 }
