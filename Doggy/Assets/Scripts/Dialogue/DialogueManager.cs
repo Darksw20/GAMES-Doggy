56b6ba4f247b01a23a885fc5dbd1c28afd2a02c9
Rodrigo Zamora <rodrigo_zamora_dav@hotmail.com>
2021-06-04 23:29:19 -0500

    Added basic dialogue code

---
+++ Doggy/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void startDialogue(Dialogue dialogue)
+    {
+        Debug.Log("Empezando diálogo con " + dialogue.name);
+
+        // Limpiar la queue y agregar todas los diálogos
+        sentences.Clear();
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        displayNextSentence();
+    }
+
+    public void displayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            endDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        Debug.Log(sentence);
+    }
+
+    private void endDialogue()
+    {
+        Debug.Log("El diálogo terminó");
+    }
+
+}
