4666e9809dd9a1e08be1dac557bf20100a2ffa79
RicardoNaVi <ricardog.navi20@gmail.com>
2021-06-25 12:17:24 -0500

    Cambio de audios por nivel

---
+++ Doggy/Assets/Scripts/Audio/AudioChanger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioChanger : MonoBehaviour
+{
+    public new AudioClip audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource musicObject = GameObject.FindGameObjectsWithTag("GameMusic")[0].GetComponent<AudioSource>();
+        musicObject.Pause();
+        musicObject.clip = audio;
+        musicObject.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
